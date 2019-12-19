8285bf29645b2613466a4be53c7cf1b5d059692e
nilawan <nilawan1256@gmail.com>
2019-12-19 09:27:58 +0700

    Kan edit 9.27

--- components/home.js
+++ components/home.js
@@ -2,29 +2,113 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  TouchableHighlight,
+  Button,
+  Alert,
+  ActivityIndicator,
+  AsyncStorage,
 } from 'react-native';
+import GetLocation from 'react-native-get-location';
 import styles from '../public/css';
 
 export default class Home extends Component {
 
-  onClickListener = () => {
-    console.log( "SHOW LOG AT HOME");
+  state = {
+    location: null,
+    loading: false,
+    name: 'null',
+  }
+
+  logout = async () => {
+    try {
+      await AsyncStorage.removeItem('@MySuperStore:key')
+    } catch(e) {
+      // remove error
+      console.log('Logout error.' + e)
+    }
+    console.log('Logout.')
+    Alert.alert('Logout');
+    this.props.navigation.navigate('auth')
+  }
+
+  _get = async () => {
+    try {
+      AsyncStorage.getItem('@MySuperStore:key')
+      .then((nameStore) => {
+        if (nameStore) {
+          this.setState({ name: nameStore });
+        }
+      });
+    } catch(e) {
+      // getdata error
+      console.log('Error. =' + e)
+    }
+  }
+
+  _requestLocation = () => {
+    this.setState({ loading: true, location: null });
+
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 6000,
+    })
+      .then(location => {
+        console.log('Location =' + JSON.stringify(location));
+        this.setState({
+          location,
+          loading: false,
+        });
+      })
+      .catch(ex => {
+        const { code, message } = ex;
+        console.warn(code, message);
+        if (code === 'CANCELLED') {
+          Alert.alert('Location cancelled by user or by another request');
+        }
+        if (code === 'UNAVAILABLE') {
+          Alert.alert('Location service is disabled or unavailable');
+        }
+        if (code === 'TIMEOUT') {
+          Alert.alert('Location request timed out');
+        }
+        if (code === 'UNAUTHORIZED') {
+          Alert.alert('Authorization denied');
+        }
+        this.setState({
+          location: null,
+          loading: false,
+        });
+      });
   }
 
   render() {
+    const { location, loading } = this.state;
+    this._get()
+
+    if(this.state.name == 'null'){
+      console.log('null')
+      this.props.navigation.navigate('auth')
+    }
+    else{
+      console.log('test1')
+    }
     return (
       <View style={styles.container}>
         <View style={styles.container}>
-          <Text style={[styles.main, styles.textShadow]}>Home Form</Text>
-          <Text style={[styles.main, styles.textShadow]}>Welcome to Homepage</Text>
+          <Text style={styles.main}>Home From</Text>
         </View>
-        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener()}>
-          <Text style={styles.loginText}>SHOW TEXT ON LOG</Text>
-        </TouchableHighlight>
-        <View>
-          <Text style={styles.backLogin} onPress={() => this.props.navigation.navigate('auth')}>Logout</Text>
+        <Text style={styles.instructions}>Welcome : {this.state.name} </Text>
+        <Text style={styles.instructions}>To get location, press the button:</Text>
+        <View style={styles.button}>
+          <Text style={styles.signupp} onPress={() => this.logout()}>Log out</Text>
+          <Button
+            disabled={loading}
+            title="Get Location"
+            onPress={this._requestLocation}
+          />
         </View>
+        {loading ? (<ActivityIndicator />) : null}
+        {location ? (<Text style={styles.location}>Latitude ={JSON.stringify(location.latitude)}</Text>) : null}
+        {location ? (<Text style={styles.location}>Longitude ={JSON.stringify(location.longitude)}</Text>) : null}
       </View>
     );
   }
